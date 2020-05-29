7521f3f228fd52bdbc7cb96c46192ed2a309164d
StrayLove <lady.stray.love@gmail.com>
2020-05-28 22:18:54 -0400

    done as far as i know of

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
-import "./App.css";
+import Header from "./components/Header";
+import PhotoCard from "./components/PhotoCard";
+import styled from 'styled-components';
+
+const Page = styled.div`
+
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
-    </div>
+    <Page>
+      <Header/>
+      <PhotoCard/>
+    </Page>
   );
 }
 
